4b8ff8468f5e111791da48376348227e6b9c9a73
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-07-10 11:06:02 -0500

    init

--- src/js/HelloWorld.js
+++ src/js/HelloWorld.js
@@ -2,12 +2,8 @@ import '../css/components.css';
 
 export const HelloWorld = () => {
     const title = document.createElement('h1');
-    const subtitle = document.createElement('h1');
-    const img = document.createElement('img');
 
-    title.innerText = `Welcome to your`;
-    img.src = 'assets/img/webpack-logo.svg';
-    subtitle.innerText = `boilerplate`;
+    title.innerText = `Todo App`;
 
-    document.body.append( title, img, subtitle );
+    document.body.append( title );
 }
