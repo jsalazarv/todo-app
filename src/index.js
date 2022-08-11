b659af7fdb96189047d811399ccd04db7ef01dbe
Juan Salazar <jsalazarv@Juans-MacBook-Pro.local>
2022-08-10 23:50:42 -0500

    Added local storage driver

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './styles.css';
 import {Todo, TodoList} from './js/classes';
-import {newTodoInput, taskCompleted, deleteTask} from './js/events'
+import {newTodoInput, taskCompleted, deleteTask, deleteCompletedTasks} from './js/events';
+import {createTodo} from "./js/components/createTodo";
 
 
 export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => createTodo(todo));
