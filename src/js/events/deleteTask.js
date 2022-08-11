146bd5d4f662aed1248c07e8a697433f093c36dc
Juan Salazar <jsalazarv@Juans-MacBook-Pro.local>
2022-08-10 21:35:17 -0500

    Added functionality to delete tasks

---
+++ src/js/events/deleteTask.js
@@ -0,0 +1,17 @@
+import {todoList} from "../../index";
+
+const deleteTask = document.querySelector('.todo-list');
+
+
+deleteTask.addEventListener('click', (event) => {
+    const item = event.target.parentElement.parentElement;
+    const clickedElement  = event.target.localName;
+    const itemId = item.getAttribute('data-id');
+
+    if(clickedElement.includes('button')) {
+        todoList.deleteTodo(itemId);
+        deleteTask.removeChild(item);
+    }
+});
+
+export default deleteTask;
--- src/js/events/taskCompleted.js
+++ src/js/events/taskCompleted.js
@@ -1,13 +1,12 @@
-import {TodoList} from "../classes";
+import {todoList} from "../../index";
+
 
 const taskCompleted = document.querySelector('.todo-list');
-
 
 taskCompleted.addEventListener('click', (event) => {
     const item = event.target.parentElement.parentElement;
     const clickedElement  = event.target.localName;
     const itemId = item.getAttribute('data-id');
-    const todoList = new TodoList();
 
     if(clickedElement.includes('input')) {
         todoList.todoStatus(itemId);
