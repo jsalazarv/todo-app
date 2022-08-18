b200b07a6ed915077d536c02baad35e2cc4c01c4
Juan Salazar <jsalazarv8@gmail.com>
2022-08-18 03:02:18 -0500

    Added filters to the list

--- src/js/events/index.js
+++ src/js/events/index.js
@@ -2,10 +2,12 @@ import newTodoInput from "./newTodoInput";
 import taskCompleted from "./taskCompleted";
 import deleteTask from "./deleteTask";
 import deleteCompletedTasks from "./deleteCompletedTasks";
+import filters from "./filters";
 
 export {
     newTodoInput,
     taskCompleted,
     deleteTask,
-    deleteCompletedTasks
+    deleteCompletedTasks,
+    filters
 }
