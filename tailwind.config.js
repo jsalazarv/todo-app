a48b921d52a82d7ea0bc5bbc1f2dc6ae014b1d62
Juan Salazar <jsalazarv8@gmail.com>
2022-08-21 15:13:18 -0500

    Added styling to input with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,28 @@
 
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          50: "#e4e5f1",
+          100: "#cacde8",
+          200: "#777a92",
+          300: "#4d5066",
+          400: "#393a4c",
+          500: "#25273c",
+          600: "#161722",
+        },
+        light: {
+          50: "#fafafa",
+          100: "#e4e5f1",
+          200: "#d2d3db",
+          300: "#9394a5",
+          400: "#484b6a",
+        }
+      },
+    },
   },
   plugins: [],
 }
