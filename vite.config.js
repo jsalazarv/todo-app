76ea689a8230f3f9e2869a2f3cffc2782f38d001
Juan Salazar <jsalazarv8@gmail.com>
2022-08-24 23:39:11 -0500

    update config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig(({command, mode}) => {
     return {
-        base: mode === "deploy" ? "./" : "/",
+        base: mode === "deploy" ? "/todo-app" : "/",
     }
 });
